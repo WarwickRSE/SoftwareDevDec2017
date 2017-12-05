e8d3480b0057eba3d91acd7d9f908b0cc1ca9974
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2017-12-05 13:10:28 +0000

    Allocation example in Fortran

---
+++ DebugIntro/Fortran/08_BadAllocation.f90
@@ -0,0 +1,32 @@
+
+PROGRAM MAIN
+
+  INTEGER, PARAMETER :: sz = 15
+  INTEGER, DIMENSION(:), ALLOCATABLE :: array
+  INTEGER, DIMENSION(:), POINTER :: ptr
+
+  !First bug
+  PRINT*, array(10)
+  array(12) = 120
+
+  !Working code
+  ALLOCATE(array(sz))
+  PRINT*, array(10)
+  array(12) = 120
+
+  !Second bug
+  ALLOCATE(array(sz))
+
+  !------------------------------
+  !Fortran Pointers
+  !Third bug
+  !PRINT*, ptr(10)
+
+  !Working code
+  !ALLOCATE(ptr(sz))
+  !PRINT*, ptr(10)
+
+
+END PROGRAM MAIN
+
+
