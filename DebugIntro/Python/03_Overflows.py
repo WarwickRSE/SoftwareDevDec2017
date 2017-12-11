185f419061a8ca5bd28eb88eb6614dc5012636c7
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2017-12-11 15:59:51 +0000

    Py3 has no maxint, only maxsize
    Also adds explicit type output for clarity

--- DebugIntro/Python/03_Overflows.py
+++ DebugIntro/Python/03_Overflows.py
@@ -6,12 +6,23 @@ if __name__ == "__main__":
 
 
   import sys
+  if sys.version_info.major == 2:
+    j = sys.maxint
+  else:
+    j = sys.maxsize
 
-  j = sys.maxint
   k = - j - 1
 
-  print("In Python normal ints are between -sys.maxint-1 of {0} and sys.maxint of {1}".format(k, j))
-  print("Adding one to maxint: {0} + 1 = {1}".format(j, j+1))
+  print("In Python 2 normal ints are between -sys.maxint-1 of {0} and sys.maxint of {1}".format(k, j))
+  print("In Python 3 ints and longs are the same thing. There is a maxsize value though")
+
+  if sys.version_info.major == 2:
+    print("Maxint is a {0}".format(type(j)))
+    print("Adding one to maxint: {0} + 1 = {1} which is {2}".format(j, j+1, type(j+1)))
+  else:
+    print("Maxsize is a {0}".format(type(j)))
+    print("Adding one to maxsize: {0} + 1 = {1} which is {2}".format(j, j+1, type(j+1)))
+
   print("Subtracting one from min : {0} - 1 = {1}".format(k, k-1))
 
   print("\nThe absolute maximum integer is limited only by memory and computational effort")
